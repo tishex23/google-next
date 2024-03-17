508a6112decfa17879dcb89de499064b47b50280
tishex23 <bitcamp124@gmail.com>
2024-03-17 19:12:17 +0400

    add image search results component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,12 +1,15 @@
 import WebSearchResults from "@/components/WebSearchResults";
+import Link from "next/link";
 
 export default async function WebSearchPage({searchParams}) {
   
-  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}`);
   const data = await response.json();
   const results = data.items;
   
-  if(!response.ok) throw new Error('Something went wrong');
+
+ 
+  
 
   if (!results) {
     return (
